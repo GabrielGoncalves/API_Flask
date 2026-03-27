9eb59ad18bb16db7875860f55a2b881ca18d6c68
Gabriel Gonçalves de Aragão <aragao.nuvem@alterdata.matriz>
2026-03-27 12:15:40 -0300

    adicionado o swagger

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from flask_pydantic_spec import FlaskPydanticSpec, Response
+from pydantic import BaseModel
 
 server = Flask(__name__)
+spec = FlaskPydanticSpec('Flask', title='Teste da API')
+spec.register(server)
+
+class MensagemRetorno(BaseModel):
+    mensagem: str
 
 @server.get('/pessoas')
+@spec.validate(resp=Response(HTTP_200=MensagemRetorno)) 
 def buscar_pessoas():
-    return 'Programaticamente Falando'
+    return {'mensagem': 'Programaticamente Falando'}
 
 server.run()
