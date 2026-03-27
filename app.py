b5a326d75c6a552904bc9f26996cea8901cd5064
Gabriel Gonçalves de Aragão <aragao.nuvem@alterdata.matriz>
2026-03-27 12:42:32 -0300

    adição do metodo post em /pessoa

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
-from flask_pydantic_spec import FlaskPydanticSpec, Response
-from pydantic import BaseModel
+from itertools import count
+from typing import Optional
+
+from flask import Flask, request, jsonify
+from flask_pydantic_spec import (
+    FlaskPydanticSpec, Response, Request
+)
+from pydantic import BaseModel, Field
 
 server = Flask(__name__)
 spec = FlaskPydanticSpec('Flask', title='Teste da API')
@@ -15,5 +20,12 @@ class Pessoa(BaseModel):
 @spec.validate(resp=Response(HTTP_200=Pessoa)) 
 def buscar_pessoas():
     return {'mensagem': 'Programaticamente Falando'}
+
+@server.post('/pessoas')
+@spec.validate(body=Request(Pessoa), resp=Response(HTTP_201=Pessoa))
+def inserir_pessoa():
+    body = request.context.body.dict()
+    return body
+    ...
 
 server.run()
