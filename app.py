bd1607a190c773dd0183205f12070a3e815db36c
Gabriel Gonçalves de Aragão <aragao.nuvem@alterdata.matriz>
2026-03-27 16:11:12 -0300

    adicionado o TinyDB

--- app.py
+++ app.py
@@ -6,10 +6,12 @@ from flask_pydantic_spec import (
     FlaskPydanticSpec, Response, Request
 )
 from pydantic import BaseModel, Field
+from tinydb import TinyDB, Query
 
 server = Flask(__name__)
 spec = FlaskPydanticSpec('Flask', title='Teste da API')
 spec.register(server)
+database = TinyDB('database.json')
 
 class Pessoa(BaseModel):
     id: int
@@ -19,12 +21,15 @@ class Pessoa(BaseModel):
 @server.get('/pessoas')
 @spec.validate(resp=Response(HTTP_200=Pessoa)) 
 def buscar_pessoas():
-    return {'mensagem': 'Programaticamente Falando'}
+    """Retorna todas as pessoas da base de dados."""
+    return jsonify(database.all())
 
 @server.post('/pessoas')
 @spec.validate(body=Request(Pessoa), resp=Response(HTTP_201=Pessoa))
 def inserir_pessoa():
+    """Insere pessoa no banco de dados."""
     body = request.context.body.dict()
+    database.insert(body)
     return body
     ...
 
