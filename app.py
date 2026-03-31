e5a6f1b896bb0a2e4ad2c1c405a1050f08c58b90
Gabriel Gonçalves de Aragão <gabriel.goncalves29@gmail.com>
2026-03-30 23:51:31 -0300

    Ajustado o delete e ID automatico

--- app.py
+++ app.py
@@ -12,12 +12,11 @@ server = Flask(__name__)
 spec = FlaskPydanticSpec('Flask', title='API com Flask')
 spec.register(server)
 database = TinyDB('database.json')
+pessoa_id = count()
 
-class Teste(BaseModel):
-    id: int
 
 class Pessoa(BaseModel):
-    id: Optional[int]
+    id: Optional[int] = Field(default_factory=lambda: next(pessoa_id))
     nome: str
     idade: int
 
@@ -50,5 +49,13 @@ def altera_pessoa(id):
     body = request.context.body.dict()
     database.update(body, Pessoa.id == id)
     return jsonify(body)
+
+@server.delete('/pessoas/<int:id>')
+@spec.validate(resp=Response('HTTP_204'))
+def deleta_pessoa(id):
+    """Remove uma pessoa do banco de dados"""
+    Pessoa = Query()
+    database.remove(Pessoa.id == id)
+    return jsonify({})
 
 server.run()
