6123ae8ae07a83cae305bc2bdea9d1891a5e1040
Gabriel Gonçalves de Aragão <gabriel.goncalves29@gmail.com>
2026-03-30 23:26:52 -0300

    criação do metodo PUT

---
+++ app.py
@@ -0,0 +1,54 @@
+from itertools import count
+from typing import Optional
+
+from flask import Flask, request, jsonify
+from flask_pydantic_spec import (
+    FlaskPydanticSpec, Response, Request
+)
+from pydantic import BaseModel, Field
+from tinydb import TinyDB, Query
+
+server = Flask(__name__)
+spec = FlaskPydanticSpec('Flask', title='API com Flask')
+spec.register(server)
+database = TinyDB('database.json')
+
+class Teste(BaseModel):
+    id: int
+
+class Pessoa(BaseModel):
+    id: Optional[int]
+    nome: str
+    idade: int
+
+class Pessoas(BaseModel):
+    pessoas: list[Pessoa]
+    count: int
+
+@server.get('/pessoas')
+@spec.validate(resp=Response(HTTP_200=Pessoas)) 
+def buscar_pessoas():
+    """Retorna todas as pessoas da base de dados."""
+    return jsonify(
+        Pessoas(pessoas=database.all(),
+                count=len(database.all())
+                ).dict()
+    )
+
+@server.post('/pessoas')
+@spec.validate(body=Request(Pessoa), resp=Response(HTTP_201=Pessoa))
+def inserir_pessoa():
+    """Insere pessoa no banco de dados."""
+    body = request.context.body.dict()
+    database.insert(body)
+    return body
+
+@server.put('/pessoas/<int:id>')
+@spec.validate(body=Request(Pessoa), resp=Response(HTTP_200=Pessoa))
+def altera_pessoa(id):
+    Pessoa = Query()
+    body = request.context.body.dict()
+    database.update(body, Pessoa.id == id)
+    return jsonify(body)
+
+server.run()
